4055a143fe5504465ae67d34873445b1503a0781
pnd Peon de la Parra Daniel <pnd@keba.com>
2021-01-29 13:41:27 +0100

    a

---
+++ ajaxBookmarklet.js
@@ -0,0 +1,24 @@
+if (window.jQuery === undefined) {
+    var j = document.createElement('script');
+    j.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js';
+    j.onload = fetchData;
+    document.body.appendChild(j);
+}
+else {
+    fetchData();
+}
+ 
+function fetchData() {
+        var rootUrl = jQuery('#ajaxBookmarklet').attr('src');
+        jQuery.ajax({
+          url: rootUrl.substr(0, rootUrl.lastIndexOf('/') + 1) + "dataExt.js",
+          type: 'get',
+          dataType: "jsonp",
+          contentType: "application/json",
+          jsonpCallback: "jsonCallback"
+  });
+}
+ 
+function jsonCallback(json){
+    alert("In jsonCallback! Got " + json.field1 + ", " + json.field2 + ", " + json.field3);
+}
